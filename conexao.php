3e6a04e541286a0f3b437a5f2a87ec513b81120f
jorge ciffoni <ciffoni@gmail.com>
2022-04-19 22:18:53 -0300

    comentario
    incluir comentario

--- conexao.php
+++ conexao.php
@@ -1,4 +1,5 @@
 ﻿<?php
+//arquivo de conexão 
     try {
         $conn = new PDO("mysql:host=localhost;dbname=telecentros","root","");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
